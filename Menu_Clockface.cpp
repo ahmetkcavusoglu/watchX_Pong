7adb7b5f0369128afd32bde5331ed5d367a3a928
Ahmet Kemal Cavusoglu <30498139+ahmetkcavusoglu@users.noreply.github.com>
2020-11-26 15:16:44 +0300

    Add files via upload

---
+++ Menu_Clockface.cpp
@@ -0,0 +1,76 @@
+#include <Arduino.h>
+#include <RTClib.h>
+#include <Adafruit_GFX.h>
+#include "Menu_Clockface.h"
+#include "Menu.h"
+#include "State.h"
+#include "Clockface_Pong.h"
+
+ClockfaceMenu::ClockfaceMenu()
+: Menu(MENU_CLOCK)
+, faceType(FACE_PONG)
+, face(NULL)
+{
+  faceType = state.current_face;
+  if (faceType >= FACE_MAX) faceType = 0;
+  changeMenu();
+  uint8_t hour = state.now.hour();
+  if (!state.mode24h && hour > 12) {
+    hour = hour - 12;
+  }
+  face->begin(hour, state.now.minute());
+}
+
+ClockfaceMenu::~ClockfaceMenu() {
+  delete face;
+}
+
+bool ClockfaceMenu::update() {
+  uint8_t hour = state.now.hour();
+  if (!state.mode24h && hour > 12) {
+    hour = hour - 12;
+  }
+  face->update(hour, state.now.minute());
+  // Always render
+  return true;
+}
+
+void ClockfaceMenu::draw(Adafruit_GFX* display) const {
+  face->draw(display);
+}
+
+void ClockfaceMenu::button1() {
+  switchMenu(MENU_SETTINGS);
+}
+
+void ClockfaceMenu::button2() {
+  switchMenu(MENU_SETTINGS);
+}
+
+void ClockfaceMenu::button3() {
+  switchMenu(MENU_SETTINGS);
+}
+
+
+void ClockfaceMenu::changeMenu() {
+  // Switch object
+  if (face) {
+    delete face;
+    face = NULL;
+  }
+  switch(faceType) {
+  case FACE_PONG:
+      face = new ClockfacePong();
+    break;
+  }
+
+  // Call begin
+  uint8_t hour = state.now.hour();
+  if (!state.mode24h && hour > 12) {
+    hour = hour - 12;
+  }
+  face->begin(hour, state.now.minute());
+
+  state.current_face = faceType;
+  state.save();
+}
